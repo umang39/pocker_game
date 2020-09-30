261021f5f3b22dd52d6ce02334b8d11f11afdaa9
Umang69 <toshugupta579@gmail.com>
2020-09-30 16:02:57 +0530

    slot machine
    Signed-off-by: Umang69 <toshugupta579@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let item1 = document.getElementById('item1')
 let item2 = document.getElementById('item2')
 let item3 = document.getElementById('item3')
-
+let round = document.getElementById('round')
 let value = ['😁','🤣','😎','😍','😘','😗','🤔']
 
 function emojiCreator(){
@@ -9,15 +9,24 @@ function emojiCreator(){
     return value[parseInt(Math.random()*7)]
 }
 let stopAnimation
+
 (function(){
+let stop = 0
+
     stopAnimation = setInterval(()=>{
+        stop++
+        console.log(stop)
+        round.value = stop
+        if(stop == 100){
+         clearInterval(stopAnimation)
+            window.alert('you lose')
+        }
         item1.innerText = emojiCreator()
         item2.innerText = emojiCreator()
         item3.innerText = emojiCreator()
         if(item1.innerText == item2.innerText && item2.innerText== item3.innerText){
          clearInterval(stopAnimation)
-         window.alert('done')   
-        }
+         window.alert('you win')}
     },300)
    
 })()
